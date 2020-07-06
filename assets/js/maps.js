7b02a5e14b4141f649cca30504d449a059406e58
Gwen-of-lynn <Gwen.of.lynn@gmail.com>
2020-07-06 20:40:02 +0000

    updated map locations to match with ' places' and started map zoomed in closer

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,6 +1,6 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
+        zoom: 6,
         center: {
             lat: 54.0000,
             lng: -7.5000
@@ -8,13 +8,10 @@ function initMap() {
     });
 
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
+    
     var locations = [
         { lat: 55.0521, lng: -7.3386 },
         { lat: 53.8023, lng: -8.3043 },
-        { lat: 55.2408, lng: -6.5116 },
-        { lat: 53.0280, lng: -7.8085 },
-        { lat: 52.1585, lng: -7.1444 }
     ];
 
     var markers = locations.map(function(location, i) {
@@ -27,4 +24,4 @@ function initMap() {
     var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 }
 
-/*Copied from CI's Map's tutorial and adapted for use on this page*/
+/*Copied from CI's Map's tutorial, edited and adapted for use on this page*/
